add vgg, fix averaging of gradients

coding.py
@@ -59,13 +59,14 @@ def uniform_decode(code, s, device=torch.device("cpu")):
 def uniform_reconstruct(grad_dict, s):
     def on_vector(v, s):
         # zero message sending simulated
-        if np.abs(np.linalg.norm(v)) < EPS:
+        if np.abs(np.linalg.norm(v)) < 1e-100:
             return v
         device = torch.device("cpu")
         if v.is_cuda:
             device = v.get_device()
         code = uniform_encode(v, s=s)
         dec = uniform_decode(code, s=s, device=device)
+        # print(f"Error: {np.linalg.norm(v - dec)}")
         # Check that zero messages remain zero
         return dec
     dec = {k: 0.0 for k in grad_dict.keys()}

models.py
@@ -5,7 +5,21 @@ import torch
 import torch.nn as nn
 
 
-class QuantizedModelBase(nn.Module):
+def get_model(model_name, *args, **kwargs):
+    """
+    User interface to models.
+    Use "vgg{11,13,16,19}"" for CIFAR10,
+    and "lenet" for MNIST
+    """
+    if "vgg" in model_name.lower():
+        return VGG(model_name.upper())
+    elif model_name == "lenet":
+        return LeNet(*args, **kwargs)
+    else:
+        raise ValueError("Unknown model {}".format(model_name))
+
+
+class ModelBase(nn.Module):
     def get_gradients(self):
         """
         Returns dictionary of layer name -> gradient wrt x mappings.
@@ -27,10 +41,10 @@ class QuantizedModelBase(nn.Module):
         self.load_state_dict(state_dict)
 
 
-class QuantizedLeNet(QuantizedModelBase):
+class LeNet(ModelBase):
     def __init__(self, in_channels=1, img_rows=28, num_classes=10):
         """ Parameters set for MNIST, change in constructor if used with other data """
-        super(QuantizedLeNet, self).__init__()
+        super(LeNet, self).__init__()
         self.model_name = 'LeNet'
         self.out_rows = ((img_rows - 4)//2 - 4)//2
         self.features = nn.Sequential(
@@ -55,3 +69,39 @@ class QuantizedLeNet(QuantizedModelBase):
         x = x.view(-1, self.out_rows*self.out_rows*50)
         x = self.classifier(x)
         return x
+
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG(ModelBase):
+    def __init__(self, vgg_name):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+